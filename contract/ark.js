df3cf6a9e02a15a3d8887ba43976e4c4df83a581
charmful0x <charmful0x@gmail.com>
2024-08-01 17:55:25 +0200

    feat: add

--- contract/ark.js
+++ contract/ark.js
@@ -2,12 +2,17 @@ export async function handle(state, action) {
     const input = action.input;
   
     if (input.function === "linkArweaveAddress") {
+      // EOA address
       const caller = lambda.msg.sender.toLowerCase();
+      // function call inputs
       const { arweave_owner, arweave_sig } = input;
-  
+      // sig verification fn takes the EOA address to use it in the
+      // sig msg creation
       await _verifyArSignature(arweave_owner, arweave_sig, caller);
+      // convert JWK to address format
       const arweave_address = await _ownerToAddress(arweave_owner);
-  
+      // EOA is the master address, it can add multiple Arweave addresses
+      // to an Ark (container of Arweave addresses linked to a unique EOA)
       if (!(caller in state.arks)) {
         state.arks[caller] = [];
       }
@@ -21,13 +26,36 @@ export async function handle(state, action) {
       return { state };
     }
   
+    if (input.function === "removeArweaveWallet") {
+      const caller = lambda.msg.sender.toLowerCase();
+      const { arweave_owner, arweave_sig } = input;
+  
+      ContractAssert(caller in state.arks, "ERROR_CALLER_NOT_FOUND");
+  
+      await _verifyArSignature(arweave_owner, arweave_sig, caller);
+      // convert JWK to address format
+      const arweave_address = await _ownerToAddress(arweave_owner);
+      const arweave_address_index_in_ark = state.arks[caller].findIndex(
+        (addr) => addr == arweave_address,
+      );
+      ContractAssert(
+        arweave_address_index_in_ark >= 0,
+        "ERROR_ARWEAVE_ADDRESS_NOT_FOUND",
+      );
+  
+      state.arks[caller].splice(arweave_address_index_in_ark, 1);
+  
+      return { state };
+    }
+  
     async function _verifyArSignature(owner, signature, evm_caller) {
       try {
         const decryptedOwner = atob(owner);
         _validatePubKeySyntax(owner);
   
         const sigBody = state.sig_messages;
-  
+        // the AR sig msg takes the message from state(state.sig_messages), decryptedOwner and EOA,
+        // all separated by "::" each
         const encodedMessage = btoa(
           `${sigBody[sigBody.length - 1]}::${decryptedOwner}::${evm_caller}`,
         );
